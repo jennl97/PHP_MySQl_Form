7ca4e5ab271438a502c2cac40811277e8fcf78c8
jennl97 <jennhott97@gmail.com>
2017-04-13 15:01:59 -0500

    Add files via upload

---
+++ form/iplsubmit.php
@@ -0,0 +1,11 @@
+<? php
+$order_number = "order_number";
+$sku = "sku";
+$pick_qty = "pick_qty";
+$qty_avaliable = "qty_avaliable";
+$item_description = "item_descrption";
+$unit = "unit";
+$bin_number = "bin_number";
+$location = "location";
+
+?>
