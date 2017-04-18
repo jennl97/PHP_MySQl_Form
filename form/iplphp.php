8a53692ce6e502e572bd13db7672483dccd8296b
jennl97 <jennhott97@gmail.com>
2017-04-18 15:18:19 -0500

    version 2

--- form/iplphp.php
+++ form/iplphp.php
@@ -10,6 +10,28 @@ define("DATABASE_USERNAME", "pmclass");
 define("DATABASE_PASSWORD", "PMclass2017");
 define("DATABASE_NAME", "pmclass_invenmgmnt");
 
-mysql_connect($database_host, $username, $password)
+$link = mysql_connect($database_host, $username, $password)
 or die("Error connecting to database" . mysql_error());
+
+$order_number = mysqli_real_escape_string($link, $_REQUEST['order_number']);
+
+$sku = mysqli_real_escape_string($link, $_REQUEST['sku']);
+
+$pick_qty = mysqli_real_escape_string($link, $_REQUEST['pick_qty']);
+
+$qty_avaliable = mysqli_real_escape_string($link, $_REQUEST['qty_avaliable']);
+
+$item_description = mysqli_real_escape_string($link, $_REQUEST['item_description']);
+
+$unit = mysqli_real_escape_string($link, $_REQUEST['unit']);
+
+$bin_number = mysqli_real_escape_string($link, $_REQUEST['bin_number']);
+
+$location = mysqli_real_escape_string($link, $_REQUEST['location']);
+
+$sql = "INSERT INTO InventoryPickList (order_number, sku, pick_qty, qty_avaliable, item_description, unit, bin_number, location ) VALUES ('$order_number', '$sku', '$pick_qty', '$qty_avaliable', '$item_description', '$unit', '$bin_number', '$location')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 ?>
